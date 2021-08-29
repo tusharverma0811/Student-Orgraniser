8ab3147ba801ee6e198278a4f5554ae2adbb71fd
tusharverma0811 <tusharverma0811@gmail.com>
2021-08-29 13:25:53 +0530

    Login/SignUp form transition issue rectified certain issues still persist

--- src/Components/LoginPopup.jsx
+++ src/Components/LoginPopup.jsx
@@ -3,42 +3,24 @@ import "../login.css";
 import ReactDom from "react-dom";
 
 export default function LoginPopup({ isOpen, close }) {
-  const [isLogin, setLogin] = React.useState(false);
+  const [isLogin, setLogin] = React.useState(true);
   if (!isOpen) return null;
-  
-
-  const loginStyle={
-            transform: isLogin?"translateX(100%)":"translateX(0%)",
-            opacity:"1",
-            zIndex:"5",
-            animation: "show 0.6s"
-  }
 
   function toggleLogin(){
-    setLogin(true);
+    setLogin((prevValue)=>{
+        return !prevValue;
+    })
   }
   
   return ReactDom.createPortal(
     <>
-    {/* const handleClick = ()=>{
-      const signUpButton= document.getElementById('signUp');
-        const signInButton= document.getElementById('signIn');
-        const Registercontainer= document.getElementById('Register-container');
-
-        signUpButton.addEventListener('click',() =>{
-            Registercontainer.classList.add("right-panel-active");
-        });
-        signInButton.addEventListener('click',() =>{
-            Registercontainer.classList.remove("right-panel-active");
-        });
-    } */}
 
       <div className="overlay">
         <div className="reg">
           <button onClick={close}>X</button>
 
 
-          <div className="Register-container form" style={isLogin ? {loginStyle} : null}>
+          <div className={isLogin?"Register-container form":"Register-container form right-panel-active"}>
 
 
           <div className="form-container sign-up-container" >
@@ -53,8 +35,6 @@ export default function LoginPopup({ isOpen, close }) {
                 <button className="styled">Sign Up with Google</button>
 
         </div>
-
-
         <div className="form-container sign-in-container">
                 <h1>Sign In</h1>
                 <input type="email" name="email" placeholder="Enter Email" required />
@@ -70,12 +50,12 @@ export default function LoginPopup({ isOpen, close }) {
                 <div className="overlay-panel overlay-left">
                     <h1>Welcome Back!</h1>
                     <p>To keep connected with us please login with your personal info</p>
-                    <button className="styled styled-ghost" id="signUp">Sign Up</button>
+                    <button className="styled styled-ghost" id="signIn"  onClick={toggleLogin}>Sign In</button>
                 </div>
                 <div className="overlay-panel overlay-right">
                     <h1>Hello!</h1>
                     <p>Enter your details and start journey with us</p>
-                    <button className="styled styled-ghost" id="signIn" onClick={toggleLogin}>Sign In</button>
+                    <button className="styled styled-ghost" id="signUp" onClick={toggleLogin}>Sign In</button>
                 </div>
             </div>
         </div>
