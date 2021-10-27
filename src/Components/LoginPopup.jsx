4587220e5b898d04e7bf484cd79863b7d5a31373
Debarghya Mukherjee <debarghyamukherjee20@gmail.com>
2021-10-27 21:22:09 +0530

    login signup popup completed as on 20.10.2021

--- src/Components/LoginPopup.jsx
+++ src/Components/LoginPopup.jsx
@@ -20,24 +20,29 @@ export default function LoginPopup({ isOpen, close }) {
   
   return ReactDom.createPortal(
     <>
-    <div className="overlay">
-    <div className="reg">
+    <div className="overlayl">
+    <div className="regl">
 <div class="main"> 
 
     <input type="checkbox" id="chk" aria-hidden="true" />
 
 			<div className="signup">
 					<label for="chk" aria-hidden="true">Sign up</label>
+          {/* <a href=""> <i class="fab fa-google fa-2x"></i></a> */}
+         
 					<input type="text" name="txt" placeholder="Enter Name" required="" />
 					<input type="email" name="email" placeholder="Enter Email" required="" />
 					<input type="password" name="pswd" placeholder="Enter Password" required="" />
           <input type="password" name="pswd" placeholder="Re-Enter Password" required="" />
 					<button>Sign up</button>
+          <h1 className="signUpOR">OR</h1>
+          <button>Sign up with Google</button>
           
 			</div>
 
 			<div className="login">
 					<label for="chk" aria-hidden="true">Login</label>
+          
 					<input type="email" name="email" placeholder="Email" required="" />
 					<input type="password" name="pswd" placeholder="Password" required="" />
 					<button>Login</button>
