fee4aa967147c6d9334e428f0410fe3f68de3f0e
Tushar Verma <63283748+tusharverma0811@users.noreply.github.com>
2022-01-14 01:07:53 +0530

    Minor Fixws in Navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import Home from './Components/Home';
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
-import MainPage from './Components/MainPage';
-import SubjectDetail from './Components/SubjectDetail';
-<<<<<<< HEAD
-import Header from "./Components/Header";
-=======
-import Navbar from './Components/Navbar';
->>>>>>> 268022f6c6ba87e068ecd5f8a47b9dac3cc4d8f0
+import React from "react";
+import Home from "./Components/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import MainPage from "./Components/MainPage";
+import SubjectDetail from "./Components/SubjectDetail";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-    
-    <Navbar></Navbar>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/main" component={MainPage}></Route>
-        <Route exact path="/navbar" component={Header}></Route>
-        <Route exact path = "/subject/:subjectid" component={SubjectDetail}></Route>
-      </Switch>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/main" component={MainPage}></Route>
+          <Route
+            exact
+            path="/subject/:subjectid"
+            component={SubjectDetail}
+          ></Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,72 @@
-import React, { useEffect, useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../Stylesheets/navbar.css";
 
 const Navbar = () => {
-    return(
-        <>
-		<div className="full">
-        <div className= "header">
+  return (
+    <>
+      <div className="full">
+        <div className="header">
+          <div classNameName="navbar">
+            <nav className="navbar navbar-expand-lg">
+              <Link className="navbar-brand">Logo</Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="fas fa-bars fa-2x"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/">
+                      Home{" "}
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                      About{" "}
+                    </Link>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <span
+                      className="nav-link dropdown-toggle"
+                      id="navbarDropdownMenuLink"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <i className="fas fa-user-circle"></i>
+                    </span>
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdownMenuLink"
+                    >
+                      <Link className="dropdown-item" >
+                        <i className="fas fa-book"></i> &nbsp;My Subjects
+                      </Link>
+                      <Link className="dropdown-item">
+                        <i className="fas fa-cog"></i> &nbsp;Edit Password
+                      </Link>
 
-<div classNameName="navbar">
-
-
-        	<nav className="navbar navbar-expand-lg">
-			<a className="navbar-brand">Logo</a>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="fas fa-bars fa-2x"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul className="navbar-nav ml-auto">
-					<li className="nav-item active">
-						<a className="nav-link" href="home1.html">Home </a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">About </a>
-					</li>
-					<li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i className = "fas fa-user-circle"></i>
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        
-              <a className="dropdown-item" href="#"><i className = "fas fa-book"></i> &nbsp;My Subjects</a>
-              <a className="dropdown-item" href="#"><i className = "fas fa-cog"></i> &nbsp;Edit Password</a>
-          
-              <a className="dropdown-item" href="#"><i className = "fas fa-sign-in-alt"></i> &nbsp;Logout</a>
-            
-            </div>
-          </li>
-				</ul>
-			</div>
-		</nav>
-
-       
-</div>
-
-
-</div>
-</div>
-
-
-        
-
-         
-        
-        </>
-    )
-}
+                      <Link className="dropdown-item">
+                        <i className="fas fa-sign-in-alt"></i> &nbsp;Logout
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
